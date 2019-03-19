de8920d57a65830b4c056e79422b8113a4f7aee2
Kyle Jennings <kylejennings@kyles-mbp.lan>
2019-03-18 21:43:09 -0400

    adding code standard and other configs

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,4 @@
 <?php
 
-
 define( 'WP_USE_THEMES', true );
-require( './wordpress/wp-blog-header.php' );
+require dirname( __FILE__ ) . '/wordpress/wp-blog-header.php';
--- public_html/wp-config.php
+++ public_html/wp-config.php
@@ -1,30 +1,31 @@
 <?php
 
-require_once dirname( dirname( __FILE__ ) ) . "/vendor/autoload.php";
+// if an env file is found lets load the vars and flag this is a local dev site.
+$env_file_dir = dirname( dirname( dirname( __FILE__ ) ) );
 
-$dotenv = new Dotenv\Dotenv( dirname( __DIR__ ) );
-$dotenv->load();
+if ( file_exists( $env_file_dir . DIRECTORY_SEPARATOR . '.env' )
+	&& file_exists( dirname( dirname( __FILE__ ) ) . '/vendor/autoload.php' )
+) {
+	include_once dirname( dirname( __FILE__ ) ) . '/vendor/autoload.php';
+	$dotenv = new Dotenv\Dotenv( $env_file_dir );
+	$dotenv->load();
+	define( 'EPI_LOCAL', true );
+}
 
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix = isset($_ENV['TABLE_PREFIX']) ? $_ENV['TABLE_PREFIX'] : 'wp_';
+$table_prefix = getenv( 'TABLE_PREFIX' ) ? getenv( 'TABLE_PREFIX' ) : 'www3_';
 
 // ** MySQL settings ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', $_ENV['DB_NAME'] );
+define( 'DB_NAME', getenv( 'DB_NAME' ) );
 
 /** MySQL database username */
-define( 'DB_USER', $_ENV['DB_USER'] );
+define( 'DB_USER', getenv( 'DB_USER' ) );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', $_ENV['DB_PASSWORD'] );
+define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) );
 
 /** MySQL hostname */
-define( 'DB_HOST', $_ENV['DB_HOST'] );
+define( 'DB_HOST', getenv( 'DB_HOST' ) );
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -35,63 +36,61 @@ define( 'DB_HOST', $_ENV['DB_HOST'] );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         $_ENV['AUTH_KEY'] );
-define( 'SECURE_AUTH_KEY',  $_ENV['SECURE_AUTH_KEY'] );
-define( 'LOGGED_IN_KEY',    $_ENV['LOGGED_IN_KEY'] );
-define( 'NONCE_KEY',        $_ENV['NONCE_KEY'] );
-define( 'AUTH_SALT',        $_ENV['AUTH_SALT'] );
-define( 'SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT'] );
-define( 'LOGGED_IN_SALT',   $_ENV['LOGGED_IN_SALT'] );
-define( 'NONCE_SALT',       $_ENV['NONCE_SALT'] );
+define( 'AUTH_KEY', getenv( 'AUTH_KEY' ) );
+define( 'SECURE_AUTH_KEY', getenv( 'SECURE_AUTH_KEY' ) );
+define( 'LOGGED_IN_KEY', getenv( 'LOGGED_IN_KEY' ) );
+define( 'NONCE_KEY', getenv( 'NONCE_KEY' ) );
+define( 'AUTH_SALT', getenv( 'AUTH_SALT' ) );
+define( 'SECURE_AUTH_SALT', getenv( 'SECURE_AUTH_SALT' ) );
+define( 'LOGGED_IN_SALT', getenv( 'LOGGED_IN_SALT' ) );
+define( 'NONCE_SALT', getenv( 'NONCE_SALT' ) );
 
 
-// load extras
-if( is_readable('./extras.php') ) {
-    require_once('./extras.php');
+if ( is_readable( './extras.php' ) ) {
+	require_once './extras.php';
 }
 
 
 // ======================================
 // Load a Memcached config if we have one
 // ======================================
-if ( file_exists( dirname( __FILE__ ) . '/memcached.php' ) )
-    $memcached_servers = include( dirname( __FILE__ ) . '/memcached.php' );
+if ( file_exists( dirname( __FILE__ ) . '/memcached.php' ) ) {
+	$memcached_servers = include dirname( __FILE__ ) . '/memcached.php';
+}
 
 
 
-// dont touch this
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', '');
+// dont touch this.
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
 
-define('WPLANG', '');
+define( 'WPLANG', '' );
 
-//Fix Uploads
-define('FS_METHOD',"direct");
+// Fix Uploads.
+define( 'FS_METHOD', 'direct' );
 
-// prevent autoupdates and plugins/theme editing
-define('WP_AUTO_UPDATE_CORE', false);
-define('AUTOMATIC_UPDATER_DISABLED', false);
-// define( 'DISALLOW_FILE_MODS', true );
+// prevent autoupdates and plugins/theme editing.
+define( 'WP_AUTO_UPDATE_CORE', false );
+define( 'AUTOMATIC_UPDATER_DISABLED', false );
 
-// turn off post revisions
+// turn off post revisions.
 define( 'WP_POST_REVISIONS', false );
 
-// Change the wp-content folder location
+// Change the wp-content folder location.
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/wp-content' );
 define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp-content' );
+define( 'WP_SITEURL', 'https://' . $_SERVER['SERVER_NAME'] . '/wordpress/' ); // WordPress Address.
+define( 'WP_HOME', 'https://' . $_SERVER['SERVER_NAME'] ); // Site Address.
 
-//
-define( 'WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress/');
 
-
-define('WP_DEBUG', true);
+define( 'WP_DEBUG', true );
 
 // ===================
 // Bootstrap WordPress
 // ===================
-if ( !defined( 'ABSPATH' ) )
-    define( 'ABSPATH', dirname( __FILE__ ) . '/wordpress/' );
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/wordpress/' );
+}
 
 
-require_once( ABSPATH . 'wp-settings.php' );
-
+require_once ABSPATH . 'wp-settings.php';
