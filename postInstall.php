61de4af3f638658ab098ce5f57570f87487bfb41
Kyle Jennings <kylejennings@kyles-mbp.lan>
2019-03-21 07:38:49 -0400

    renamed and refactored some things

--- postInstall.php
+++ postInstall.php
@@ -1,120 +1,119 @@
 <?php
 
 namespace wordpressInit;
-require_once "./vendor/autoload.php";
+
+require_once './vendor/autoload.php';
 use Dotenv;
 
-$dotenv = new Dotenv\Dotenv( dirname(__DIR__) );
+$dotenv = new Dotenv\Dotenv( dirname( __DIR__ ) );
 $dotenv->load();
 
-class postInstall {
-    function __construct() {
-        
-    }
+class PostInstall {
 
-    static public function init()
-    {
-        self::cloneRepo();
-        self::runComposer();
-        self::moveRootFiles();
-    }
+	public static function init() {
+		self::cloneRepo();
+		self::runComposer();
+		self::moveRootFiles();
+	}
 
 
-    /**
-     * Download the repo from the env file
-     * @return [type] [description]
-     */
-    static public function cloneRepo() {
+	/**
+	 * Download the repo from the env file
+	 * @return [type] [description]
+	 */
+	public static function cloneRepo() {
 
-        echo "Cloning the repo \n";
-        $dst = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'public_html';
-        if( !isset($_ENV['GITHUB']) || is_readable($dst . '/wp-content') )
-            return;
+		echo "Cloning the repo \n";
+		$dst = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'public';
+		if ( ! isset( $_ENV['GITHUB'] ) || is_readable( $dst . '/content' ) ) {
+			return;
+		}
 
-        $cmd = 'cd ' .  $dst;
-        $cmd .= ' git clone '. $_ENV['GITHUB'] . ' wp-content';
-        shell_exec($cmd);
-    }
-
-
-    /**
-     * run composer on the downloaded repo
-     * @return [type] [description]
-     */
-    static public function runComposer() 
-    {
-
-        echo "Running composer on downloaded project \n";
-        $cmd = 'cd ' .  dirname(__FILE__) . DIRECTORY_SEPARATOR . 'public_html/wp-content;';
-        $cmd .= ' composer install -n';
-
-        shell_exec($cmd);
-    }
+		$cmd  = 'cd ' . $dst;
+		$cmd .= ' git clone ' . $_ENV['GITHUB'] . ' content';
+		shell_exec( $cmd );
+	}
 
 
-    /**
-     * Move any files from the "root" folder up to public_html
-     *
-     * These are usuall just a htaccess file, and webapp stuff
-     * @return [type] [description]
-     */
-    static public function moveRootFiles() {
+	/**
+	 * run composer on the downloaded repo
+	 * @return [type] [description]
+	 */
+	public static function runComposer() {
 
-        $src = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'public_html/wp-content/root';
-        $dst = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'public_html';
+		echo "Running composer on downloaded project \n";
+		$cmd  = 'cd ' . dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'public/content;';
+		$cmd .= ' composer install -n';
 
-        echo "Moving applicable files to public_html (web root) \n";
-
-        if(!is_readable($src)){
-            echo('no root folder found.');
-            return;
-        }
-
-        self::copy(
-            $src,
-            $dst
-        );
-    }
+		shell_exec( $cmd );
+	}
 
 
-    /**
-     * Recursivily copy files
-     * @param  string $src path/to/src_file
-     * @param  string $dst path/to/dest
-     */
-    static public function copy( $src, $dst ) {
-        if( !defined('DS') ) define( 'DS', DIRECTORY_SEPARATOR );
+	/**
+	 * Move any files from the "root" folder up to public
+	 *
+	 * These are usuall just a htaccess file, and webapp stuff
+	 * @return [type] [description]
+	 */
+	public static function moveRootFiles() {
 
-        $dir = opendir( $src );
+		$src = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'public/content/root';
+		$dst = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'public';
 
-        // make the destination folder if it doesnt exist
-        if(!is_readable($dst)){
-            mkdir( $dst, 0777, true );
-        }
+		echo "Moving applicable files to public (web root) \n";
 
-        while( false !== ( $file = readdir( $dir ) ) ) {
-            if( $file != '.' && $file != '..' ) {
-                if( is_dir( $src . DS . $file ) ) {
-                    self::copy( $src . DS . $file, $dst . DS . $file );
-                } else {
-                    if(is_readable($src . DS . $file))
-                        copy( $src . DS . $file, $dst . DS . $file );
-                }
-            }
-        }
-        closedir( $dir );
-    }
+		if ( ! is_readable( $src ) ) {
+			echo 'no root folder found.';
+			return;
+		}
+
+		self::copy(
+			$src,
+			$dst
+		);
+	}
 
 
-    /**
-     * Helper function to examine an object or array
-     */
-    static public function examine($val = array(), $mode = null)
-    {
-        if( empty($val) && $mode != 'vardump' )
-            return;
-        echo "<pre>";
-        print_r($val);
-        die;
-    }
+	/**
+	 * Recursivily copy files
+	 * @param  string $src path/to/src_file
+	 * @param  string $dst path/to/dest
+	 */
+	public static function copy( $src, $dst ) {
+		if ( ! defined( 'DS' ) ) {
+			define( 'DS', DIRECTORY_SEPARATOR );
+		}
+
+		$dir = opendir( $src );
+
+		// make the destination folder if it doesnt exist
+		if ( ! is_readable( $dst ) ) {
+			mkdir( $dst, 0777, true );
+		}
+
+		while ( false !== ( $file = readdir( $dir ) ) ) {
+			if ( $file !== '.' && $file !== '..' ) {
+				if ( is_dir( $src . DS . $file ) ) {
+					self::copy( $src . DS . $file, $dst . DS . $file );
+				} else {
+					if ( is_readable( $src . DS . $file ) ) {
+						copy( $src . DS . $file, $dst . DS . $file );
+					}
+				}
+			}
+		}
+		closedir( $dir );
+	}
+
+
+	/**
+	 * Helper function to examine an object or array
+	 */
+	public static function examine( $val = array(), $mode = null ) {
+		if ( empty( $val ) && $mode !== 'vardump' )
+			return;
+		echo '<pre>';
+		print_r( $val );
+		die;
+	}
 }
--- public_html/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-define( 'WP_USE_THEMES', true );
-require dirname( __FILE__ ) . '/wordpress/wp-blog-header.php';
